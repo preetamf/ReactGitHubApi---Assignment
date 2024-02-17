c09d98fb1fe60eb3c41d1bdbabd58a88238d1fce
preetamf <preetamsinghe2202@gmail.com>
2024-02-17 11:28:02 +0530

    initial setup commit

---
+++ github-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      {`Hello world!
+      Let's Build the Project`}
+    </h1>
+  )
+}
+
+export default App
